a7c484cc4acd099ea891274a06031d2758ea5c7b
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-04-26 17:12:43 +0530

    add

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const Address = require("../model/address.model");
-const Resident = require("../model/data.model");
+const Data = require("../model/data.model");
 
 const router = express.Router();
 
 router.post("/post", async (req, res) => {
   try {
-    const data = await Resident.findOne({addressId : req.body.addressId}).lean().exec();
+    const data = await Data.findOne({addressId : req.body.addressId}).lean().exec();
     if(data){
       return res.status(300).json({message : "user already added the data"});
     }
-    const playlist = await Resident.create(req.body);
+    const playlist = await Data.create(req.body);
     return res.status(200).json(playlist);
   } catch (err) {
     return res.status(400).json(err.message);
@@ -18,11 +18,11 @@ router.post("/post", async (req, res) => {
 });
 router.get('/all', async (req, res) => {
   try {
-    const residentData = await Resident.find()
+    const DataData = await Data.find()
     .populate({path : 'addressId'})
     .lean()
     .exec();
-    return res.status(200).json(residentData);
+    return res.status(200).json(DataData);
   }
   catch(err) {
     return res.status(400).json(err.message);
@@ -33,8 +33,8 @@ router.get("/:id", async (req, res) => {
   try {
     const flatID = req.params.id;
     const address = await Address.findOne({userId: flatID}).select({city : 1, capacity : 1, rating : 1, houseUrl : 1}).lean().exec();
-    const userPlaylist = await Resident.findOne({addressId : flatID}).populate([{path : 'addressId', select : ["firstName", "lastName", "type"]}]).select({flatId : 0}).lean().exec();
-    const final = {...address, ...userPlaylist}
+    const Data2 = await Data.findOne({addressId : flatID}).populate([{path : 'addressId', select : ["firstName", "lastName", "type"]}]).select({flatId : 0}).lean().exec();
+    const final = {...address, ...Data2}
 
     return res.status(200).json(final);
 
@@ -42,10 +42,22 @@ router.get("/:id", async (req, res) => {
     return res.status(400).json(err.message);
   }
 });
-router.patch('/:id', async (req, res) => {
+router.patch('/reqtolist/:id', async (req, res) => {
   try {
-    const resident = await Resident.findByIdAndUpdate(req.params.id, req.body, {new : true}).lean().exec();
-    return res.status(200).json(resident);
+    let query = req.query.key || 0;
+    let Data2 = await Data.findById(req.params.id).lean().exec();
+    let redDd = await Data2.reqPets[query] !== undefined || null ? Data2.reqPets[query] : 'z';
+    let pet = await Data2.Pets || [];
+    if(redDd === 'z') {
+      return res.status(200).json({message : "this pet already admitted"})
+    }
+    Data2 = {...Data2, reqPets : Data2.reqPets.filter((p)=> {
+      return p !== redDd
+    }), 
+      Pets :  pet.length>0 ? [...pet, redDd] : [redDd]
+    }
+    const finalData = await Data.findByIdAndUpdate({_id : req.params.id}, Data2, {new : true})
+    return res.status(200).json({finalData});
   }
   catch(err) {
     return res.status(400).json(err.message)
--- src/controllers/pet.controller.js
+++ src/controllers/pet.controller.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Data = require('../model/data.model')
 const Pet = require('../model/pet.model');
 router.get('/all', async(req,res)=>{
     try {
@@ -13,7 +14,8 @@ router.get('/all', async(req,res)=>{
 router.post('/post', async(req,res)=>{
     try {
         const pet = await Pet.create(req.body);
-        return res.status(200).json(pet);
+        const data = await Data.findOneAndUpdate({userId : pet.userId[1]}, {reqPets : [pet._id]}, {new : true} ).lean().exec();
+        return res.status(200).send({pet, data});
     }
     catch (err) {
         return res.status(500).json(err);
